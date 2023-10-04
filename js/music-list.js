84195f90e824bf78efd65b78781807b4ed1970cb
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-04 14:20:42 +0530

    music playlist added

---
+++ js/music-list.js
@@ -0,0 +1,164 @@
+let allMusic = [
+  {
+    name: "Jennie - Solo",
+    artist: "Jennie",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "BlackPink - So Hot",
+    artist: "BlackPink",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "BlackPink - Pink Venom",
+    artist: "BlackPink",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "DJ_Snake & LISA - Sexy Girl",
+    artist: "DJ_Snake & LISA",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "BlackPink - Kill This Love",
+    artist: "BlackPink",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "BlackPink - Don't Know what",
+    artist: "BlackPink",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "BlackPink - Kick it",
+    artist: "BlackPink",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "BlackPink - Hope Not",
+    artist: "BlackPink",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "BP & LadyGaga - Sour Candy",
+    artist: "BP & LadyGaga",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "LISA - Lalisa",
+    artist: "LISA",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "LISA - Money",
+    artist: "LISA",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "BlackPink - Whistle",
+    artist: "BlackPink",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "BlackPink - Boombayah",
+    artist: "BlackPink",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "BlackPink - Playing With Fire",
+    artist: "BlackPink",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "BlackPink - Stay",
+    artist: "BlackPink",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "BlackPink - Ddu-du-ddu-du",
+    artist: "BlackPink",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "BlackPink - Forever Young",
+    artist: "BlackPink",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "BlackPink - Really",
+    artist: "BlackPink",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "BlackPink - See U Later",
+    artist: "BlackPink",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "BlackPink - How You Like That",
+    artist: "BlackPink",
+    img: "music-20",
+    src: "music-20"
+  },
+  {
+    name: "BlackPink & Selina - Ice Cream",
+    artist: "BlackPink & Selina",
+    img: "music-21",
+    src: "music-21"
+  },
+  {
+    name: "BlackPink - Pretty Savage",
+    artist: "BlackPink",
+    img: "music-22",
+    src: "music-22"
+  },
+  {
+    name: "BlackPink - Bet You Wanna",
+    artist: "BlackPink",
+    img: "music-23",
+    src: "music-23"
+  },
+  {
+    name: "BlackPink - Lovesick Girls",
+    artist: "BlackPink",
+    img: "music-24",
+    src: "music-24"
+  },
+  {
+    name: "BlackPink - Crazy Over You",
+    artist: "BlackPink",
+    img: "music-25",
+    src: "music-25"
+  },
+  {
+    name: "BlackPink - Love To Hate Me",
+    artist: "BlackPink",
+    img: "music-26",
+    src: "music-26"
+  },
+  {
+    name: "BlackPink - You Never Know",
+    artist: "BlackPink",
+    img: "music-27",
+    src: "music-27"
+  },
+];
